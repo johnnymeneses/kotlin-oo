6ef6c648fa9b0281733a26b48c42d9357b0daf71
johnnymeneses <bilbo_vader@outlook.com>
2021-09-08 18:32:46 -0300

    Lista Lógica

---
+++ ListaLogica/src/exemplos.kt
@@ -0,0 +1,15 @@
+
+
+fun declaracaoVariaveis()
+{
+//    Variável com valor imutável
+    val nome = "Johnny"
+
+//    Variável com valor que pode ser modificado
+    var nome2 = "Johnny"
+    var nome3: String
+}
+
+
+
+
