0f8e2cf34bb769345f6316cdf59ff8ef92c3fcc4
johnnymeneses <bilbo_vader@outlook.com>
2021-09-11 11:42:53 -0300

    Encapsulamento

--- ListaLogica/src/main.kt
+++ ListaLogica/src/main.kt
@@ -1,12 +1,8 @@
 
 fun main()
 {
-//    val banco = Empresa("Banco XYZ","1234534","Endereco da Empresa",5000.0)
-//    val otica = Empresa("Otica","6565656","Rua da Empresa",4500.0)
-//
-//    banco.imprimir()
-//    otica.imprimir()
-
+    val resultado = Divisao()
+    println("10 / 2 = ${resultado.dividir(10,2)}")
 }
 
 
@@ -22,9 +18,36 @@ class Empresa (
         println("Endereço: $endereco")
         println("Faturamento: $faturamento")
     }
+
+    //    val banco = Empresa("Banco XYZ","1234534","Endereco da Empresa",5000.0)
+//    val otica = Empresa("Otica","6565656","Rua da Empresa",4500.0)
+//
+//    banco.imprimir()
+//    otica.imprimir()
+
 }
 
 
+class Divisao{
+    var num1: Int=0
+        private set
+    var num2: Int=0
+        private set
+
+    fun dividir (num1: Int, num2: Int) : Double
+    {
+        this.num1 = num1
+        this.num2 = num2
+
+        if(num2==0){
+            return 0.0
+        }else {
+
+            var resultado = (num1 / num2).toDouble()
+            return resultado
+        }
+    }
 
 
+}
 
