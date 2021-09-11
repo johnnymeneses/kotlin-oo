d24ffc72266ab3a572191493b6ed91467351eabb
johnnymeneses <bilbo_vader@outlook.com>
2021-09-11 11:28:07 -0300

    Ex OO

--- ByteBank/src/contaCorrente.kt
+++ ByteBank/src/contaCorrente.kt
@@ -1,3 +1,8 @@
+import java.util.*
+
+
+
+
 class contaCorrente(titular: String, agencia: Int, numeroConta: Int) : Conta (titular, agencia, numeroConta)
 {
 
--- ListaLogica/src/main.kt
+++ ListaLogica/src/main.kt
@@ -1,7 +1,27 @@
 
 fun main()
 {
-    println("Imobiliaria 10 andar")
+//    val banco = Empresa("Banco XYZ","1234534","Endereco da Empresa",5000.0)
+//    val otica = Empresa("Otica","6565656","Rua da Empresa",4500.0)
+//
+//    banco.imprimir()
+//    otica.imprimir()
+
+}
+
+
+class Empresa (
+    val nome: String,
+    val cnpj: String,
+    val endereco: String,
+    val faturamento: Double)
+{
+    fun imprimir(){
+        println("Nome: $nome")
+        println("CNPJ: $cnpj")
+        println("Endereço: $endereco")
+        println("Faturamento: $faturamento")
+    }
 }
 
 
