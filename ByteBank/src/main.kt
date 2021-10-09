ab50d591f0cd9885d74cdecc988860a15b34eb9b
johnnymeneses <bilbo_vader@outlook.com>
2021-10-09 18:24:42 -0300

    Exceptions

--- ByteBank/src/main.kt
+++ ByteBank/src/main.kt
@@ -1,19 +1,47 @@
 import br.com.johnny.bytebank.modelo.Conta
+import br.com.johnny.bytebank.modelo.Endereco
 
 
 fun main()
 {
-    println("Bem vindo ao BytBank - Kotlin Edition. ")
+//    println("Bem vindo ao BytBank - Kotlin Edition. ")
+
 
 //    testaFuncionarios()
 //    testaAutenticacao()
 //    testaContas()
 //    testaEndereco()
 //    testaCodigoNovo()
-    testaCriaContas()
+//    testaCriaContas()
+
+    mainTeste()
 
 
+}
 
+fun mainTeste(){
+    println("início da main")
+    funcao1()
+    println("fim da main")
+}
+
+
+fun funcao1(){
+    println("início da funcao1")
+    funcao2()
+    println("fim da funcao1")
+
+}
+fun funcao2(){
+    println("início da funcao2")
+    for (i in 1..5){
+        println(i)
+
+        val endereco = Any()
+        endereco as Endereco
+    }
+
+    println("fim da funcao2")
 
 }
 
