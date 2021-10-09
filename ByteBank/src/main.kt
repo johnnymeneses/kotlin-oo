c70a64e09f6f28d0bba3b9c980f99d690ea2b966
johnnymeneses <bilbo_vader@outlook.com>
2021-10-09 19:09:54 -0300

    Exceptions

--- ByteBank/src/main.kt
+++ ByteBank/src/main.kt
@@ -2,6 +2,7 @@ import br.com.johnny.bytebank.modelo.Conta
 import br.com.johnny.bytebank.modelo.Endereco
 import java.beans.Expression
 import java.lang.ClassCastException
+import java.lang.Double.parseDouble
 import java.lang.Integer.parseInt
 import java.lang.NumberFormatException
 
@@ -25,6 +26,49 @@ fun main()
 
 fun mainTeste(){
     println("início da main")
+
+    val entrada: String ="3"
+
+
+//    try{
+//        val valor: Double = entrada.toDouble()
+//        println("Valor recebido $valor")
+//    }catch (e: NumberFormatException){
+//        println("Problem na conversão")
+//        e.printStackTrace()
+//    }
+
+//    val a: Int? = try { parseInt("d")} catch (e: NumberFormatException) {null}
+
+    val valorRecebido: Double? = try{
+        entrada.toDouble()
+    }catch (e: NumberFormatException){
+        println("deu ruim")
+        e.printStackTrace()
+        null
+    }
+
+//    if as expression
+    val valorComTaxa: Double? = if(valorRecebido != null){
+        valorRecebido+0.1}
+    else{
+        null
+        }
+
+
+    if(valorComTaxa != null)
+    {
+        println("valor recebido: $valorRecebido")
+    }else {
+        println("valor invalido $valorRecebido")
+    }
+
+
+
+
+
+
+
     funcao1()
     println("fim da main")
 }
