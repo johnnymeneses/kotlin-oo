8a5d6a5db628346fb0d9ffc327da288a27b3037d
johnnymeneses <bilbo_vader@outlook.com>
2021-10-10 16:10:23 -0300

    Estudos sobre Any

--- ByteBank/src/main.kt
+++ ByteBank/src/main.kt
@@ -2,7 +2,7 @@ import br.com.johnny.bytebank.modelo.Endereco
 
 
 fun main() {
-        println("Bem vindo ao BytBank - Kotlin Edition. ")
+        println("Bem vindo ao ByteBank - Kotlin Edition. ")
 
 //    testaFuncionarios()
 //    testaAutenticacao()
@@ -11,20 +11,8 @@ fun main() {
 //    testaCodigoNovo()
 //    testaCriaContas()
 //    testaTry()
-//
-    val endereco = Endereco(numero=20, logradouro="rua de baixo", cep = "000000")
-    val  enderecoNovo = Endereco(numero= 20, logradouro = "rua de baixo", cep = "000000")
+//      testaAny()
 
-//   equals: indica se algum dos objetos é igual ao outro
-    println(endereco.equals(enderecoNovo)) //false
-    println(endereco.equals(endereco)) //true
-
-
-    println(endereco.hashCode())//2093631819
-    println(enderecoNovo.hashCode())//2074407503
-
-        println(endereco.toString()) //br.com.johnny.bytebank.modelo.Endereco@7cca494b
-        println(enderecoNovo.toString()) //br.com.johnny.bytebank.modelo.Endereco@7ba4f24f
 
 
 
