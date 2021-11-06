1058417cb034083dec0cb4318795a9af46a75fd4
johnnymeneses <bilbo_vader@outlook.com>
2021-11-06 11:16:30 -0300

    Estrutura de Dados

--- EstruturaDados/src/main/kotlin/main.kt
+++ EstruturaDados/src/main/kotlin/main.kt
@@ -1,4 +1,20 @@
     fun main(){
         println("Estrutura de Dados")
+
+        val idades = IntArray(4)
+         idades[0] = 25
+         idades[1] = 35
+         idades[2] = 45
+         idades[3] = 15
+
+        //Percorrendo o array
+
+        for(idade in idades)
+        {
+            println(idades[idade])
+        }
+
+
+
     }
 
