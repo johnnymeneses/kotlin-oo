687d6c606ed1f35fb7289413e5424cf8609c7fee
johnnymeneses <bilbo_vader@outlook.com>
2021-11-07 12:10:44 -0300

    Aula Estrutura de Dados - Big Decimal

--- EstruturaDados/src/main/kotlin/SalarioBigDecimal.kt
+++ EstruturaDados/src/main/kotlin/SalarioBigDecimal.kt
@@ -6,31 +6,7 @@ fun SalarioBigDecimal() {
     println("Salario Big Decimal")
 
 
-
-    //To.do funcionŕio deve ter um auvmento de 10%. Se o aumento for menor que R$500,00.. cravar o valor em R$ 500,00
-    val salarioEmpresa = bigDecimalArrayOf("3000.0","4000.0","5000.0","6000.0","7000.0")
-
-    //Aumento de 10%
-    //Aumento de R$ 500,003
-    //Somente dois valores depois da virgula e arrendondar pra cima
-
-    val aumento = "1.1".toBigDecimal() //Aumento de 10%
-    val salarioAumento: Array<BigDecimal> = salarioEmpresa
-//        .map {salarioEmpresa -> (salarioEmpresa * aumento).setScale(2,RoundingMode.UP)} Opção para tudo
-        .map { salarioEmpresa ->
-            if(salarioEmpresa<"5000.0".toBigDecimal())
-            {
-                salarioEmpresa+"500.0".toBigDecimal()
-            }else{
-                (salarioEmpresa * aumento).setScale(2,RoundingMode.UP)
-            }
-        }
-        .toTypedArray()
-
-
-
-    println(salarioEmpresa.contentToString())
-    println(salarioAumento.contentToString())
+    regraNegocio()
 
 
 }
@@ -65,4 +41,42 @@ fun exemploDinamico(){
     val salariosDinamico = bigDecimalArrayOf("3500.0","2500.0","8500.0","500.0")
     println(salariosDinamico.contentToString())
 
+}
+
+
+fun regraNegocio(){
+    //To.do funcionŕio deve ter um auvmento de 10%. Se o aumento for menor que R$500,00.. cravar o valor em R$ 500,00
+    val salarioEmpresa = bigDecimalArrayOf("3000.0","4000.0","5000.0","6000.0","7000.0")
+
+    //Aumento de 10%
+    //Aumento de R$ 500,003
+    //Somente dois valores depois da virgula e arrendondar pra cima
+
+    val aumento = "1.1".toBigDecimal() //Aumento de 10%
+    val salarioAumento: Array<BigDecimal> = salarioEmpresa
+//        .map {salarioEmpresa -> (salarioEmpresa * aumento).setScale(2,RoundingMode.UP)} Opção para tudo
+        .map { salarioEmpresa ->
+            calculaAumentoRelativo(salarioEmpresa, aumento)
+        }
+        .toTypedArray()
+
+
+    println(salarioEmpresa.contentToString())
+    println(salarioAumento.contentToString())
+
+
+
+
+
+
+
+}
+
+private fun calculaAumentoRelativo(
+    salarioEmpresa: BigDecimal,
+    aumento: BigDecimal
+) = if (salarioEmpresa < "5000.0".toBigDecimal()) {
+    salarioEmpresa + "500.0".toBigDecimal()
+} else {
+    (salarioEmpresa * aumento).setScale(2, RoundingMode.UP)
 }
