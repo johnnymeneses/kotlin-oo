529bb029e0c6ad6eafeed9c0c390abffe93791a3
johnnymeneses <bilbo_vader@outlook.com>
2021-11-14 17:25:35 -0300

    Referencia Kotlin

--- kotlin-referencia/src/main/kotlin/estruturaRepeticao.kt
+++ kotlin-referencia/src/main/kotlin/estruturaRepeticao.kt
@@ -1,7 +1,13 @@
 fun controleFLuxo(){
 
-//    IF
-//    FOR
+//FOR
+    tabuada(5)
+
+//WHEN
+    novelos(6)
+
+
+
 //    DO WHILE
 //    WHILE SO
 
@@ -9,5 +15,24 @@ fun controleFLuxo(){
 
 
 
+
+}
+
+//FOR
+fun tabuada (num: Int){
+    for(index in 1..10)
+    {
+        println("$index * $num = " + index * num)
+    }
+}
+
+//WHEN
+fun novelos(blusa: Int){
+
+    when {
+        blusa <= 0 -> return
+        blusa == 1 -> println("Para $blusa blusa, é preciso ${blusa*87} novelos")
+        blusa > 1 -> println("Para $blusa blusas, é preciso ${blusa*87} novelos")
+    }
 
 }
---
+++ kotlin-referencia/src/main/kotlin/funcoes.kt
@@ -0,0 +1,23 @@
+fun funcoes(){
+
+    //retorno direto
+    areaTerreno(5,6)
+
+    //função com parametro
+    qtdeDias(5,1)
+
+}
+
+
+//Função com retorno direto
+fun areaTerreno(a: Int, b: Int): Int = a*b
+
+
+
+//Função com parametro
+fun qtdeDias(dia: Int, mes: Int)
+{
+    var total = ((mes-1)*30)+dia
+
+    println("Já se passaram $total dias")
+}
