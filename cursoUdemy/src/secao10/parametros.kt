0584babcbd8c78e56f848484df8b2bcc5ef0726f
johnnymeneses <bilbo_vader@outlook.com>
2021-11-28 12:47:18 -0300

    Secao 10

--- cursoUdemy/src/secao10/parametros.kt
+++ cursoUdemy/src/secao10/parametros.kt
@@ -2,7 +2,8 @@ package secao10
 
 fun parametros() {
 
-    endereco("Nome Rua","São Paulo","SP","01014000","145")
+//    endereco("Nome Rua","São Paulo","SP","01014000","145")
+    endereco(cidade = "São Paulo", estado="SP",cep="54654")
 
 }
 
@@ -10,7 +11,7 @@ fun parametros() {
 //Em alguns casos, as funções podem receber muitos paaremtros. Como lidar com o caso de ser necessário manter
 //todos os parametros , mas nao ter todos disponíveis na chamada: receber um valor Default
 
-fun endereco(rua: String="", cidade: String, estado: String, cep: String, numero: String=""){
+fun endereco(rua: String="Rua", cidade: String, estado: String, cep: String, numero: String="1515"){
     println("Rua $rua")
     println("Cidade: $cidade, $estado, - $cep")
 
