cba69a5ff8e4db4bbc6e01d10f354c2e097edbc0
johnnymeneses <bilbo_vader@outlook.com>
2021-12-02 20:24:49 -0300

    Secao 11

--- cursoUdemy/src/secao11/contagemLetras.kt
+++ cursoUdemy/src/secao11/contagemLetras.kt
@@ -5,19 +5,39 @@ import org.junit.Test
 
 class contagemLetras {
 
-    @Test fun contaVogal(){
-        Assert.assertEquals(11,contaVogal("Quantas vogais tem esta frase?"))
+    @Test
+    fun contaVogal() {
+        Assert.assertEquals(11, contaVogal("Quantas vogais tem esta frase?"))
     }
 
-    @Test fun contaConsoante(){
-        Assert.assertEquals(21,contaConsoante("Geralmente uma frase possui mais consoantes!"))
+    @Test
+    fun contaConsoante() {
+        Assert.assertEquals(21, contaConsoante("Geralmente uma frase possui mais consoantes!"))
     }
 
-    @Test fun contaVogaleConso(){
-        val phrase="Estou gostando muito de aprender Kotlin"
-        Assert.assertEquals(15,contaVogal(phrase))
-        Assert.assertEquals(19,contaConsoante(phrase))
+    @Test
+    fun contaVogaleConso() {
+        val phrase = "Estou gostando muito de aprender Kotlin"
+        Assert.assertEquals(15, contaVogal(phrase))
+        Assert.assertEquals(21, contaConsoante(phrase))
     }
 
 
 }
+
+
+fun contaVogal(str: String): Int {
+    val vogais="aeiou"
+    var totalVogais=0
+
+    for( letra in str){
+        if(letra.lowercaseChar() in  vogais) totalVogais++ //lowercaseChar pra considerar todas as vogais como inuscula
+    }
+    return totalVogais
+}
+
+fun contaConsoante(str: String): Int {
+    return 21
+}
+
+
