ba942938f59d2996d17bb7f453530524be6c74dd
johnnymeneses <bilbo_vader@outlook.com>
2021-12-05 15:05:08 -0300

    Secao 14- Herança

---
+++ cursoUdemy/src/secao14/heranca.kt
@@ -0,0 +1,38 @@
+//Computador pode ser uma especialização de máquina
+//um carro pode ser uma maquina
+//um trator pode ser uma maquina
+
+//depois de definitir os atributos, adicionar :
+//quando vc herda uma classe, vc herda o que a classe pai espera
+//a classe pai, para ser herdada precisa da palavra reservada OPEN.
+
+
+open class Maaquina(val marca: String) {
+    fun minhaMarca() {
+        println("minha marca é $marca")
+    }
+}
+
+class Computador(marca: String, val nucleos: Int) : Maaquina(marca) {
+    fun ligar() {
+
+    }
+
+    fun processar() {
+    }
+
+
+}
+
+fun heranca() {
+
+    val dell = Computador("Dell", 15)
+
+    with(dell){
+        ligar()
+        println("liguei")
+        processar()
+        println("processei")
+        minhaMarca()
+    }
+}
--- cursoUdemy/src/secao14/secao14.kt
+++ cursoUdemy/src/secao14/secao14.kt
@@ -1,5 +1,12 @@
 package secao14
 
+import heranca
+
 fun secao14(){
     println("Secao 14")
+
+    heranca()
+
+
+
 }
