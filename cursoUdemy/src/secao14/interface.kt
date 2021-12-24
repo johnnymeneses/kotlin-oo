28af585f288b432468e96a465b996385cb1c0e0f
johnnymeneses <bilbo_vader@outlook.com>
2021-12-24 12:19:38 -0300

    Interfaces

--- cursoUdemy/src/secao14/interface.kt
+++ cursoUdemy/src/secao14/interface.kt
@@ -1,20 +1,51 @@
 package secao14
 
 // Uma interface define um contrato, quem herda deve assinar
+//interface nao consegue ter estado. nao consegue ter astribuição de variavel
 
-interface Maquina{
+interface Maquina {
+
     fun ligar()
+    fun desligar()
 }
 
-class Computador: Maquina{
+class Computador : Maquina {
     override fun ligar() {
-        TODO("Not yet implemented")
+        println("Ligando Dell")
+    }
+
+    override fun desligar() {
+        println("Desligando Dell")
     }
 
 }
 
 
-fun mainInterface(){
+class Trator : Maquina{
+    override fun ligar() {
+        println("Ligando Trator")
+    }
+
+    override fun desligar() {
+        println("Desligando Trator")
+    }
+}
+
+
+
+
+fun mainInterface() {
     println("Interfaces")
+
+    val dell = Computador()
+    val ibec = Trator()
+
+    dell.ligar()
+    dell.desligar()
+
+    ibec.ligar()
+    ibec.desligar()
+
+
 
 }
