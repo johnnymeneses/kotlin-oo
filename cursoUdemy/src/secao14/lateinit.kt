dccd4f311791c7a3ea1c4a9373e6fcc1d2e6a985
johnnymeneses <bilbo_vader@outlook.com>
2021-12-25 10:01:21 -0300

    Interfaces

--- cursoUdemy/src/secao14/lateinit.kt
+++ cursoUdemy/src/secao14/lateinit.kt
@@ -1,16 +1,18 @@
 ///Inicializacao taria
 
+class Pessoa{
 
+    //Property must be initialized or be abstract (sem o lateinit)
+    lateinit var nome: String
 
+    fun geradorDeNome(){
+        nome = "eoeoekoke"
+    }
 
-
-
-
-
-
-
-
+}
 
 fun iniciaTardia() {
 
+    val p = Pessoa()
+    p.geradorDeNome()
 }
