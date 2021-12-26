f845007fa1cfab813cf813c18e35d60670d5df4f
johnnymeneses <bilbo_vader@outlook.com>
2021-12-26 20:23:53 -0300

    Refactor Lista Logica

--- ListaLogica/src/logica/Ex03.kt
+++ ListaLogica/src/logica/Ex03.kt
@@ -1,4 +1,10 @@
 package logica
 //Impar ou Par ?
 
-    fun ex03(num: Int): String = if (num % 2 == 0) "$num é Par" else "$num é Impar"
+
+fun main() {
+    println(ex03(5))
+}
+
+
+fun ex03(num: Int): String = if (num % 2 == 0) "$num é Par" else "$num é Impar"
--- ListaLogica/src/logica/Ex04.kt
+++ ListaLogica/src/logica/Ex04.kt
@@ -1,4 +1,8 @@
 package logica
 // Divisão de dois números
 
-    fun ex04(num1:Double, num2: Double): Double = num1 / num2.toDouble()
+
+fun main(){
+    println(ex04(10,5))
+}
+    fun ex04(num1:Int, num2: Int): Double = num1 / num2.toDouble()
