cd4509f95e8019174a1e9fd5a928002a23d8e980
johnnymeneses <bilbo_vader@outlook.com>
2022-01-23 17:40:50 -0300

    Kotlin

---
+++ ListaLogica/src/logica/ExTreinaEntrevista.kt
@@ -0,0 +1,23 @@
+package logica.logica
+
+// Declarar Variavel mutável
+//Declarar varia´vel imut[abel
+//IF
+//Laços de Repetição
+//Orientação a objeto
+
+fun main(){
+
+
+
+    val nome ="Johnny"
+    val idade="15"
+    val anoNascimento: Int = idade.toInt()
+
+        println("Variavel comum: $nome")
+        println("Cast de String pra Int: $idade ${anoNascimento+10}")
+
+}
+
+
+
