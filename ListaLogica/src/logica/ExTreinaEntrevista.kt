d75d33f0d76d0cc2f65a8641949f7f294ed2d1f0
johnnymeneses <bilbo_vader@outlook.com>
2022-01-23 18:47:51 -0300

    Kotlin

--- ListaLogica/src/logica/ExTreinaEntrevista.kt
+++ ListaLogica/src/logica/ExTreinaEntrevista.kt
@@ -1,75 +1,28 @@
 package logica.logica
 
-//Orientação a objeto
-//Criar um Funcionario > Gerente Herdando de Funcionario > Gerente Abstraindo de Fucnionario > Analista> Calcula Bonificação
+import java.io.PipedOutputStream
 
-abstract class Funcionario(val nome: String, var salario: Double) {
+class Pedido(
+    val id: Int,
+    val desc: String,
+    val qtde: Int
+) {
 
-    open fun salarioBonus(nome: Funcionario) {
-        salario += 200
+    fun imprimir() {
+        println("ID: $id")
+        println("Descrição: $desc")
+        println("Quantidade: $qtde")
     }
 }
 
-
-class Gerente(nome: String, salario: Double) : Funcionario(nome, salario) {
-
-    override fun salarioBonus(nome: Funcionario) {
-        salario+=800
-    }
-}
-
-class Analista(var nomeA: String, var salarioA: Double) : Funcionario(nomeA, salarioA) {
-
-}
+fun main(){
 
 
-fun main() {
+    val vendaUm = Pedido(1, "tesoura", 4)
+    val vendaDois = Pedido(2, "Lapiseira", 1)
+    val vendaTres = Pedido(3, "Canetas", 10)
 
-    val johnny = Analista("Johnny", 500.0)
-    val eder = Gerente("Eder",800.0)
-
-    println(johnny.salario)
-    johnny.salarioBonus(johnny)
-    println(johnny.salario)
-
-    println(eder.salario)
-    eder.salarioBonus(eder)
-    println(eder.salario)
-
-
-}
-
-
-fun basicoUm() {
-
-    val nome = "Johnny"
-    val idade = "15"
-    val anoNascimento: Int = idade.toInt()
-    var mudaValor = 4
-
-    mudaValor = 10
-
-    while (mudaValor > 1) {
-        println(mudaValor)
-        mudaValor--
-    }
-    println("Variavel comum: $nome")
-    println("Cast de String pra Int: $idade ${anoNascimento + 10}")
-    println(mudaValor)
-
-    if (mudaValor <= 18) {
-        println("menorIdade")
-    } else {
-        println("Maior de idade")
-    }
-
-}
-
-private fun tabuada(num: Int) {
-
-    for (i in 1..10) {
-        println(i * num)
-    }
-
-
+    vendaUm.imprimir()
+    vendaDois.imprimir()
+    vendaTres.imprimir()
 }
